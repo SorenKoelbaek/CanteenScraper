c9863c26b830379db3097c6debb6c32524100141
sorenkoelbaek <soren@sorenkoelbaek.com>
2024-01-12 15:56:30 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status
 from routers import menu
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Lego Menu Backend",
@@ -8,6 +8,20 @@ app = FastAPI(
     version="0.1.0",
     #docs_url=None,
     #redoc_url=None
+)
+
+origins = [
+    "http://localhost:8000",
+    "http://canteen.sorenkoelbaek.dk",
+    "https://canteen.sorenkoelbaek.dk",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(menu.router)
